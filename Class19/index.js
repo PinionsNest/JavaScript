57109e77b4ce073636d3f01881d069fbf2a3237e
PinionsNest <135445676+PinionsNest@users.noreply.github.com>
2023-06-21 00:59:49 +0600

    aa

--- Class19/index.js
+++ Class19/index.js
@@ -1,36 +1,39 @@
-import { REST, Routes, Client, GatewayIntentBits } from 'discord.js';
+import "dotenv/config";
+import { REST, Routes, Client, GatewayIntentBits } from "discord.js";
 
 const commands = [
   {
-    name: 'whoareyou',
-    description: 'Replies with Pong!',
+    name: "whoareyou",
+    description: "Replies with Pong!",
   },
 ];
 
-const rest = new REST({ version: '10' }).setToken("MTEyMDc0ODUyMDQwMDQ5NDY2Mw.GtqKnn.6ljDa49EttM1uoMWLQNuFwcFEqyo0LOQAIOGKE");
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 try {
-  console.log('Started refreshing application (/) commands.');
+  console.log("Started refreshing application (/) commands.");
 
-  await rest.put(Routes.applicationCommands("1120748520400494663"), { body: commands });
+  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+    body: commands,
+  });
 
-  console.log('Successfully reloaded application (/) commands.');
+  console.log("Successfully reloaded application (/) commands.");
 } catch (error) {
   console.error(error);
 }
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
+client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === 'whoareyou') {
-    await interaction.reply('You can call me big daddy!');
+  if (interaction.commandName === "whoareyou") {
+    await interaction.reply("Success!");
   }
 });
 
-client.login("MTEyMDc0ODUyMDQwMDQ5NDY2Mw.GtqKnn.6ljDa49EttM1uoMWLQNuFwcFEqyo0LOQAIOGKE");
+client.login(process.env.TOKEN);
